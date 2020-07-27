2d14295bc6d5e48e309205703c6b70de37e2eac0
AleksandrBelykov <68788562+AleksandrBelykov@users.noreply.github.com>
2020-07-27 18:42:33 +0200

    Add files via upload

---
+++ Task_0217.java
@@ -0,0 +1,30 @@
+
+public class Task_0217 {
+
+	public static int min(int a, int b, int c, int d) {
+        return min(min(min(a, b), c), d);
+        //напишите тут ваш код
+
+}
+
+public static int min(int a, int b) {
+int e;
+if (a<b)
+e = a;
+ else
+ e = b;
+ 
+ return e;
+ //напишите тут ваш код
+
+}
+
+public static void main(String[] args) throws Exception {
+ System.out.println(min(-20, -10));
+ System.out.println(min(-40, -10, -30, 40));
+ System.out.println(min(-20, -40, -30, 40));
+ System.out.println(min(-20, -10, -40, 40));
+ System.out.println(min(-20, -10, -30, -40));
+	}
+
+}
